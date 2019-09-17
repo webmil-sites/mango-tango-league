ded7e850a06f944ff1d44506df9f1bc2908a5f01
Taras Chuiko <taraschuiko@gmail.com>
2019-09-17 15:37:28 +0300

    fixed mobile league switching in schedule

--- js/index.js
+++ js/index.js
@@ -9,28 +9,10 @@ buttonNonPro.addEventListener("click", function() {
   buttonSolo.classList.remove("button-schedule-solo");
   buttonNonPro.classList.add("button-schedule");
 
-  document
-    .getElementById("headlineColor")
-    .classList.remove("color-t-vivid-red");
-  document
-    .getElementById("headlineColor")
-    .classList.add("color-t-yellow--mobile");
-
-  document
-    .getElementById("paragraphColor")
-    .classList.add("color-t-yellow--mobile");
-  document
-    .getElementById("paragraphColor")
-    .classList.remove("color-t-vivid-red");
-
   if (contentNonPro.classList.contains("js-hide")) {
     contentSolo.classList.add("js-hide");
     contentSolo.classList.remove("js-show");
-    contentSoloBottom.classList.add("js-hide");
-    contentSoloBottom.classList.remove("js-show");
-
     contentNonPro.classList.add("js-show");
-    contentNonProBottom.classList.add("js-show");
   }
 });
 
@@ -38,24 +20,10 @@ buttonSolo.addEventListener("click", function() {
   buttonSolo.classList.add("button-schedule-solo");
   buttonNonPro.classList.remove("button-schedule");
 
-  document
-    .getElementById("headlineColor")
-    .classList.remove("color-t-yellow--mobile");
-  document.getElementById("headlineColor").classList.add("color-t-vivid-red");
-
-  document
-    .getElementById("paragraphColor")
-    .classList.remove("color-t-yellow--mobile");
-  document.getElementById("paragraphColor").classList.add("color-t-vivid-red");
-
   if (contentSolo.classList.contains("js-hide")) {
     contentNonPro.classList.add("js-hide");
     contentNonPro.classList.remove("js-show");
-    contentNonProBottom.classList.add("js-hide");
-    contentNonProBottom.classList.remove("js-show");
-
     contentSolo.classList.add("js-show");
-    contentSoloBottom.classList.add("js-show");
   }
 });
 
@@ -63,12 +31,8 @@ const removeClasses = function() {
   if (window.innerWidth > 1080) {
     contentNonPro.classList.remove("js-show");
     contentNonPro.classList.remove("js-hide");
-    contentSolo.classList.remove("js-show");
-    contentSolo.classList.remove("js-hide");
   } else if (window.innerWidth < 1080) {
     contentSolo.classList.add("js-hide");
   }
 };
 removeClasses();
-
-console.log("start");
